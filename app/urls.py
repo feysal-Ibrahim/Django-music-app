9b176f883e9d65d789857961fef2616f89feeec5
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-14 19:14:06 +0300

    user register

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,10 @@ urlpatterns=[
     # /app/
     url( r'^$' , views.IndexView.as_view( ) , name='index' ) ,
 
+    # /app/register
+
+    url( r'^register/$' , views.UserForm.as_view( ) , name='register' ) ,
+
     # /app/<album_id>/
     url( r'^(?P<pk>[0-9]+)/$' , views.DetailView.as_view( ) , name='detail' ) ,
 
@@ -14,13 +18,13 @@ urlpatterns=[
     url( r'album/add/$' , views.AlbumCreate.as_view( ) , name='album-add' ) ,
 
     # /app/album/2/
-    url( r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view( ) , name='album-update' ),
-
+    url( r'album/(?P<pk>[0-9]+)/$' , views.AlbumUpdate.as_view( ) , name='album-update' ) ,
 
     # /app/album/2/delete
-    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view( ) , name='album-delete' ) ,
+    url( r'album/(?P<pk>[0-9]+)/delete/$' , views.AlbumDelete.as_view( ) , name='album-delete' ) ,
+
 
 ]
 
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns+=static( settings.MEDIA_URL , document_root=settings.MEDIA_ROOT )
